15072e4790078b682acae8294fc4ec0f657122bd
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-01-21 14:07:08 +0600

    added loading animation for the button and added the animation in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,13 +64,17 @@ export default {
           from: { opacity: "0" },
           to: { opacity: "1" },
         },
+        flashing: {
+          "0%, 100%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+        },
       },
       animation: {
         marquee: "marquee var(--marquee-duration) linear infinite",
         "fade-in": "fade-in 0.5s linear forwards",
+        flashing: "flashing 1.4s infinite linear",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-  
 } satisfies Config;
