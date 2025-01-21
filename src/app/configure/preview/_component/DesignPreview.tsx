e45aef827916c219ee84c42860f994c8df28858e
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-01-21 12:17:33 +0600

    added some static and dynamic data rendering to the preview page

--- src/app/configure/preview/_component/DesignPreview.tsx
+++ src/app/configure/preview/_component/DesignPreview.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Phone from "@/components/Phone";
-import { cn } from "@/lib/utils";
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { cn, formatPrice } from "@/lib/utils";
 import { COLORS, MODELS } from "@/validator/option-validator";
 import { Configuration } from "@prisma/client";
+import { Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
 
@@ -12,7 +14,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
   useEffect(() => setShowConfetti(true));
 
-  const { color, model } = configuration;
+  const { color, model, finish, material } = configuration;
   const tw = COLORS.find(
     (supportedColor) => supportedColor.value === color
   )?.value;
@@ -20,6 +22,14 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const { label: modelLabel } = MODELS.options.find(
     ({ value }) => value === model
   )!;
+
+  let totalPrice = BASE_PRICE;
+  if (material === "polycarbonate") {
+    totalPrice += PRODUCT_PRICES.materials.polycarbonate;
+  }
+  if (finish === "textured") {
+    totalPrice += PRODUCT_PRICES.finish.textured;
+  }
 
   return (
     <>
@@ -32,18 +42,82 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 90 }}
         />
       </div>
-      <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+      <div className="my-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
-            className={cn(
-              `${{
-                "bg-zinc-900": tw === "black",
-                "bg-blue-950": tw === "blue",
-                "bg-rose-950": tw === "rose",
-              }}`
-            )}
+            className={cn({
+              "bg-zinc-900": tw === "black",
+              "bg-blue-950": tw === "blue",
+              "bg-rose-950": tw === "rose",
+            })}
             imgSrc={configuration.croppedImageUrl as string}
           />
+        </div>
+        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-900">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorbtion</li>
+                <li>Packaging made of recycled materials</li>
+                <li>2 years of print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High quality durable material</li>
+                <li>Scratch and fingerprint resistant coating</li>
+              </ol>
+            </div>
+          </div>
+          <div className="mt-8">
+            <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+              <div className="flow-root text-sm">
+                <div className="flex items-center justify-between py-1 mt-2">
+                  <p className="text-gray-600">Base Price</p>
+                  <p className="font-medium text-gray-900">
+                    {formatPrice(BASE_PRICE / 100)}
+                  </p>
+                </div>
+                {finish === "textured" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Textured Finish</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
+                    </p>
+                  </div>
+                ) : null}
+                {material === "polycarbonate" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Soft polycarbonate material</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(
+                        PRODUCT_PRICES.materials.polycarbonate / 100
+                      )}
+                    </p>
+                  </div>
+                ) : null}
+                <div className=" bg-gray-200 my-2 h-px" />
+                <div className="flex items-center justify-between py-2">
+                  <p className="font-semibold text-gray-900">Order total</p>
+                  <p className="font-semibold text-gray-900">
+                    {formatPrice(totalPrice / 100)}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
