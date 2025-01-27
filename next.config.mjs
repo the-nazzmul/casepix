52482f663eee578d5a3a4ce83e819ff34ec98a36
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-01-27 20:17:37 +0600

    added new image hostname for uploadthing

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "utfs.io",
       },
+      {
+        protocol: "https",
+        hostname: "dk7d1vh7h3.ufs.sh",
+      },
     ],
   },
 };
