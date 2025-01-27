5452cc1cb04ca95682d63c4c30ace918168513b7
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-01-27 20:34:16 +0600

    added new resend.com domain

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "CasePix <nazmul.hussain.utchash@gmail.com>",
+        from: "CasePix <support@casepix.app>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
